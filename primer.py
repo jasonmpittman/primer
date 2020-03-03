8c946a5f02bf85c4387ac6fc8cad9338553f8cb9
cameron meadows <cmeadows@highpoint.edu>
2020-03-02 20:15:34 -0500

    almost dict created

--- primer.py
+++ primer.py
@@ -1,6 +1,6 @@
 import npyscreen
 import curses
-
+import os
 
 
 class MainForm(npyscreen.FormWithMenus):
@@ -9,10 +9,10 @@ class MainForm(npyscreen.FormWithMenus):
     #store values of the form
     self.parentApp.selectedServices = self.serviceBox.get_selected_objects()     
     self.parentApp.honeypotInfo = self.honeypotInfoBox.values
-    f = open("demofile2.txt", "a")
-    f.write(str(self.serviceBox.get_selected_objects()))
-    f.write(str(self.honeypotInfoBox.values))
-    f.close()
+  #  f = open("demofile2.txt", "a")
+  #  f.write(str(self.serviceBox.get_selected_objects()))
+  #  f.write(str(self.honeypotInfoBox.values))
+  #  f.close()
     #here is where we would store all the information about the values selected/input
     #build the string or whatever, and then store it, and then run it against the honeypot
     self.parentApp.setNextForm('PCAP')
@@ -25,13 +25,9 @@ class MainForm(npyscreen.FormWithMenus):
             max_width=x // 2 - 5)
 
 
-    #read in the config file for the service list
-    #MOVE THIS INTO ITS OWN FUNCTION
-    f = open("config/services.csv", "r")
-    services =f.read().split(",")
-
+   
     #add the list of services, using the service object
-    self.serviceBox = self.add(npyscreen.MultiSelect, name="Services", footer="footer", values = services, scroll_exit = True,
+    self.serviceBox = self.add(npyscreen.MultiSelect, name="Services", footer="footer", values = self.parentApp.services, scroll_exit = True,
             relx=x // 2, rely=2)  
 
 
@@ -45,20 +41,45 @@ class PcapForm(npyscreen.Form):
     return
 
   def create(self):
+    for i in self.parentApp.selectedServices:
+      f = open("demofile2.txt", "a")
+      f.write(str(i))
+      f.close()
+      self.add(npyscreen.TitleText, name=str(i))
     self.myName        = self.add(npyscreen.TitleText, name='Name')
     self.myDepartment  = self.add(npyscreen.TitleMultiSelect, scroll_exit=True, max_height=3, name='Department', values = ['Department 1', 'Department 2', 'Department 3'])
     self.myDate        = self.add(npyscreen.TitleDateCombo, name='Date Employed')
 
 
+#The form that will display the pcaps selected as well as the ip address entered
+class RunForm(npyscreen.Form):
+  def afterEditing(self):
+    self.parentApp.setNextForm(PcapForm)
+
+  def beforeEditing(self):
+    #hgffgh
+    return
+  
+  def create(self):
+
+    return
 
 #Parent Class that controls the application with form data, etc
 class Primer(npyscreen.NPSAppManaged):
-  selectedServices, honeypotIP, pcaps = None, None, None
+  selectedServices = []
+  honeypotIP, pcaps = None, None
+  #read in the config file for the service list
+  #MOVE THIS INTO ITS OWN FUNCTION
+  f = open("config/services.csv", "r")
+  services = f.read().split(",")
+  
+  pcaps = os.listdir(path='pcap/')
+  
   def onStart(self):
     #add the forms we need
     self.addForm('MAIN', MainForm, name='PRIMER')
     self.addForm('PCAP', PcapForm, name='PCAP')
-    #self.addForm('RUN', RunForm, name = 'RUN')
+    self.addForm('RUN', RunForm, name = 'RUN')
 
 
 #running the app
