e085337642c4db47339955e2cac5ee9f34fd905d
Jason M. Pittman <jason@jasonmpittman.com>
2020-09-22 17:56:40 -0400

    updated classes to match refactor diagram

---
+++ bin/networking.py
@@ -0,0 +1,44 @@
+
+
+class Networking():
+
+_HostMac
+_HostIp
+_TargetMac
+_TargetIp
+_Interfaces
+
+
+@property
+host_mac(self):
+    return self._HostMac
+
+host_mac.setter(self, value):
+    self._HostMac = value
+
+@property
+host_ip(self):
+    return self._HostIp
+
+host_ip.setter(self, value):
+
+
+@property
+target_mac(self):
+
+
+target_mac.setter(self):
+
+
+@property
+target_ip(self):
+
+
+target_ip.setter(self, value):
+
+
+@property
+interfaces(self):
+
+
+interfaces.setter(self, value):
