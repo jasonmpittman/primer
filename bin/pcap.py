2f5b68b6ebb6f6c73a8c3b24986df68e854d01fe
MetaphoricalGnat <nathan.markle21@gmail.com>
2020-09-29 17:41:51 -0400

    generic changes

--- bin/pcap.py
+++ bin/pcap.py
@@ -1,27 +1,32 @@
 
 class pcap():
-  _name()
-  _sourceIp()
-  _destinationIp()
-  _service()
+  def __init__(self, newName):
+    self._name(newName)
+    self._sourceIp()
+    self._destinationIp()
+    self._service()
+    self.readConfig()
 
   @property
   def name(self):
     return self._name
-  
+
   def name.setter(self, value):
     self._name = value
 
   @property
   def sourceIp(self):
     return self._sourceIp
-  
+
   def sourceIp.setter(self, value):
     self._sourceIp = value
 
   @property
   def destinationIp(self):
     return self._destinationIp
-  
+
   def destinationIp.setter(self, value):
     self._destinationIp = value
+
+  def readConfig(self):
+      open("../config/pcapInfo.csv")
--- bin/pcaps.py
+++ bin/pcaps.py
@@ -1,15 +1,21 @@
-
+import pcap
 class pcaps():
-  _pcaps[]
+  def __init__(self):
+    self._pcaps = [None]
 
   @property
   def pcaps(self):
     return self._pcaps
-  
+
   def __pcaps.setter(self, pcap):
     self.__pcaps = pcap
 
   def getPcaps():
+    return __pcaps
+
+  def createPcaps():
     #read in the pcaps
     pcaps = os.listdir(path='pcap/')
-    return pcaps
+    for x in pcaps:
+      #create a new object with the name of the pcap file x
+      newPcap = pcap(x)
